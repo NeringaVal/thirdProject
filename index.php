0a3dee350e5ea6a300f4a2c6924c3377dc5eca97
neringaVal <neringutev@gmail.com>
2022-12-03 20:59:53 +0200

    padarius 01

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+print_r($_GET);
+
+if(isset($_GET['color'])){
+    $backgroundColorPage="red";
+
+  
+}
+else {
+    $backgroundColorPage="black";
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            background-color: <?php echo $backgroundColorPage; ?>;
+            color: antiquewhite;
+        }
+        a {
+            color: azure;
+        }        
+        </style>
+</head>
+<body>
+    <h1>Užduotis 7.01 </h1>
+    <a href="./index.php">tas pats page</a>
+    <a href="./index.php?color=1">tas pats page su kintamuoju</a>
+</body>
+</html>
