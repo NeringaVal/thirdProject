b03fc2fa7cc5ce11c3b4c4776f3aa59945358322
neringaVal <neringutev@gmail.com>
2022-12-04 16:31:49 +0200

    padaryta 07

---
+++ index6.php
@@ -0,0 +1,45 @@
+<?php
+
+echo $_SERVER['REQUEST_METHOD'];
+
+if($_SERVER['REQUEST_METHOD'] == "POST")
+    {
+    $backgroundColorPage="yellow";
+    header("location: ./index6.php");
+    die;
+  
+}
+if($_SERVER['REQUEST_METHOD'] == "GET")
+{
+    $backgroundColorPage="green";
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            background-color: <?php echo $backgroundColorPage; ?>;
+            color: antiquewhite;
+        }
+        a {
+            color: azure;
+        }        
+        </style>
+</head>
+<body>
+    <h1>Užduotis 7.07 </h1>
+    <form action="" method="get">
+        <button type="submit">get color</button>
+    </form>
+    <form action="" method="post">
+        <button type="submit">post color</button>
+    </form>
+</body>
+</html>
